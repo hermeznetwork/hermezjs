3113ee73cacfca65bc49d56ed45b5849536868d8
David Ruiz <druiz0992@gmail.com>
2020-12-13 19:22:49 +0100

    Token tests

--- tests/token.test.mjs
+++ tests/token.test.mjs
@@ -4,56 +4,31 @@ import * as providers from '../src/providers.js'
 import ERC20ABI from '../src/abis/ERC20ABI.js'
 import ethers from 'ethers'
 
-test('Tx without approve', async () => {
-  // 1. ERC20 Token deployed
-  // 2. Account 0 created and funded
-  // 3. Send Transfer from account 0 to 1 without previous approval => It should fail
-
+test('Check Allowance', async () => {
+  /*
+   * 1 - Deploy contracts
+   * 2 - Approve Hermez contract amount1 of ERC tokens
+   * 3 - Check allowance
+   */
   const txAddress = '0xc783df8a850f42e7f7e57013759c285caa701eb6'
-  const rxAddress = '0xead9c93b79ae7c1591b1fb5323bd777e86e150d4'
-  const contractAddress = '0xf4e77E5Da47AC3125140c470c71cBca77B5c638c'
-
-  const amount = utils.getTokenAmountBigInt('900000', 2)
+  const hermezAddress = '0x500D1d6A4c7D8Ae28240b47c8FCde034D827fD5e'
+  const erc20Address = '0xf4e77E5Da47AC3125140c470c71cBca77B5c638c'
 
   // Initialize providers
   providers.setProvider('http://localhost:8545')
   const provider = providers.getProvider()
 
+  // Get erc20 contract
   const signer = provider.getSigner(txAddress)
-  const contract = new ethers.Contract(contractAddress, ERC20ABI, signer)
+  const erc20Contract = new ethers.Contract(erc20Address, ERC20ABI, signer)
 
-  try {
-    await contract.transferFrom(txAddress, rxAddress, amount)
-  } catch (e) {
-    expect(e.message.includes('SERVER_ERROR')).toBe(true)
+  // Set allowance to Hermez contract
+  for (var i = 0; i < 10; i++) {
+    const amountStr = Math.floor(Math.random() * 10000).toString()
+    const amount = utils.getTokenAmountBigInt(amountStr, 2)
+    await tokens.approve(amount, txAddress, erc20Address)
+    const allowance = await erc20Contract.allowance(txAddress, hermezAddress)
+
+    expect(allowance.toString()).toBe(amount.toString())
   }
-})
-
-test('Tx insufficient approve', async () => {
-  // 1. ERC20 Token deployed
-  // 2. Account 0 created and funded
-  // 3. Send Transfer from account 0 to 1 wit insufficient approval
-
-  const txAddress = '0xc783df8a850f42e7f7e57013759c285caa701eb6'
-  const rxAddress = '0xead9c93b79ae7c1591b1fb5323bd777e86e150d4'
-  const contractAddress = '0xf4e77E5Da47AC3125140c470c71cBca77B5c638c'
-
-  // TODO: I can only work with 0 ammount
-  // const txAmount = utils.getTokenAmountBigInt('900000', 2)
-  // const approveAmount = utils.getTokenAmountBigInt('800000', 2)
-  const txAmount = utils.getTokenAmountBigInt('0', 2)
-  const approveAmount = utils.getTokenAmountBigInt('0', 2)
-
-  // Initialize providers
-  providers.setProvider('http://localhost:8545')
-  const provider = providers.getProvider()
-
-  const signer = provider.getSigner(txAddress)
-  const contract = new ethers.Contract(contractAddress, ERC20ABI, signer)
-
-  await tokens.approve(approveAmount, txAddress, contractAddress)
-  await contract.transferFrom(txAddress, rxAddress, txAmount)
-
-  const rxBalance = await contract.balanceOf(rxAddress)
-  expect(rxBalance.toString()).toBe(approveAmount.toString())
 })
