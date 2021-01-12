2aae98027421df96aeb3198e6d4c728748e5d7bc
Alberto Elias <hi@albertoelias.me>
2021-01-12 17:09:04 +0100

    Adds enum for contract names

--- src/constants.js
+++ src/constants.js
@@ -10,6 +10,11 @@ const GAS_LIMIT = 5000000
 
 const GAS_MULTIPLIER = 1
 
+const ContractNames = {
+  Hermez: 'Hermez',
+  WithdrawalDelayer: 'WithdrawalDelayer'
+}
+
 const contractAddresses = {
   Hermez: '0x10465b16615ae36F350268eb951d7B0187141D3B',
   WithdrawalDelayer: '0x8EEaea23686c319133a7cC110b840d1591d9AeE0'
@@ -21,7 +26,7 @@ const contractAddresses = {
  * @private
  */
 function setContractAddress (name, address) {
-  contractAddresses[name] = address
+  contractAddresses[ContractNames[name]] = address
 }
 
 const DEFAULT_PAGE_SIZE = 20
@@ -36,6 +41,7 @@ export {
   GAS_LIMIT,
   GAS_MULTIPLIER,
   DEFAULT_PAGE_SIZE,
+  ContractNames,
   contractAddresses,
   BASE_API_URL,
   setContractAddress as _setContractAddress
