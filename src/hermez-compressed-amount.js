0e0b8210dcf87f36fc740830d6c0cf8852851db1
Alberto Elias <hi@albertoelias.me>
2021-02-11 10:34:32 +0100

    Moves to float40

--- src/hermez-compressed-amount.js
+++ src/hermez-compressed-amount.js
@@ -33,41 +33,16 @@ class HermezCompressedAmount {
       throw new Error('The parameter needs to be an instance of HermezCompressedAmount created with HermezCompressedAmount.compressAmount')
     }
     const fl = hermezCompressedAmount.value
-    const m = (fl & 0x3FF)
-    const e = (fl >> 11)
-    const e5 = (fl >> 10) & 1
+    const m = (fl % 0x800000000)
+    const e = Math.floor(fl / 0x800000000)
 
     let exp = Scalar.e(1)
     for (let i = 0; i < e; i++) {
       exp *= Scalar.e(10)
     }
 
-    let res = Scalar.mul(m, exp)
-    if (e5 && e) {
-      res = Scalar.add(res, Scalar.div(exp, 2))
-    }
-    return res
-  }
+    const res = Scalar.mul(m, exp)
 
-  /**
-   * Convert a fix to a float, always rounding down
-   * @param {String} _f - Scalar encoded in fix
-   * @returns {Scalar} Scalar encoded in float
-   * @private
-  */
-  static _floorCompressAmount (_f) {
-    const f = Scalar.e(_f)
-    if (Scalar.isZero(f)) return 0
-
-    let m = f
-    let e = 0
-
-    while (!Scalar.isZero(Scalar.shr(m, 10))) {
-      m = Scalar.div(m, 10)
-      e++
-    }
-
-    const res = Scalar.toNumber(m) + (e << 11)
     return res
   }
 
@@ -78,40 +53,27 @@ class HermezCompressedAmount {
   */
   static compressAmount (_f) {
     const f = Scalar.e(_f)
-
-    function dist (n1, n2) {
-      const tmp = Scalar.sub(n1, n2)
-
-      return Scalar.abs(tmp)
+    if (Scalar.isZero(f)) {
+      return new HermezCompressedAmount(0)
     }
 
-    const fl1 = HermezCompressedAmount._floorCompressAmount(f)
-    const fi1 = HermezCompressedAmount.decompressAmount(new HermezCompressedAmount(fl1))
-    const fl2 = fl1 | 0x400
-    const fi2 = HermezCompressedAmount.decompressAmount(new HermezCompressedAmount(fl2))
+    let m = f
+    let e = 0
 
-    let m3 = (fl1 & 0x3FF) + 1
-    let e3 = (fl1 >> 11)
-    if (m3 === 0x400) {
-      m3 = 0x66 // 0x400 / 10
-      e3++
-    }
-    const fl3 = m3 + (e3 << 11)
-    const fi3 = HermezCompressedAmount.decompressAmount(new HermezCompressedAmount(fl3))
-
-    let res = fl1
-    let d = dist(fi1, f)
-
-    const d2 = dist(fi2, f)
-    if (Scalar.gt(d, d2)) {
-      res = fl2
-      d = d2
+    while (Scalar.isZero(Scalar.mod(m, 10)) && (!Scalar.isZero(Scalar.div(m, 0x800000000)))) {
+      m = Scalar.div(m, 10)
+      e++
     }
 
-    const d3 = dist(fi3, f)
-    if (Scalar.gt(d, d3)) {
-      res = fl3
+    if (e > 31) {
+      throw new Error('number too big')
     }
+
+    if (!Scalar.isZero(Scalar.div(m, 0x800000000))) {
+      throw new Error('not enough precision')
+    }
+
+    const res = Scalar.toNumber(m) + (e * 0x800000000)
 
     return new HermezCompressedAmount(res)
   }
@@ -123,16 +85,24 @@ class HermezCompressedAmount {
   */
   static floorCompressAmount (_f) {
     const f = Scalar.e(_f)
-
-    const fl1 = HermezCompressedAmount._floorCompressAmount(f)
-    const fl2 = fl1 | 0x400
-    const fi2 = HermezCompressedAmount.decompressAmount(new HermezCompressedAmount(fl2))
-
-    if (Scalar.leq(fi2, f)) {
-      return new HermezCompressedAmount(fl2)
-    } else {
-      return new HermezCompressedAmount(fl1)
+    if (Scalar.isZero(f)) {
+      return 0
     }
+
+    let m = f
+    let e = 0
+
+    while (!Scalar.isZero(Scalar.div(m, 0x800000000))) {
+      m = Scalar.div(m, 10)
+      e++
+    }
+
+    if (e > 31) {
+      throw new Error('number too big')
+    }
+
+    const res = Scalar.toNumber(m) + (e * 0x800000000)
+    return new HermezCompressedAmount(res)
   }
 }
 
