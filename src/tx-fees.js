5b9cf08f000e64bbc8a4abc0f346fa224f7d2efe
Adri <19190248+adrifdez@users.noreply.github.com>
2021-12-22 12:03:58 +0100

    add optional params

--- src/tx-fees.js
+++ src/tx-fees.js
@@ -75,9 +75,9 @@ async function estimateWithdrawGasLimit (token, merkleSiblingsLength, amount, ov
  * @param {Object} token - The token information object as returned from the API
  * @param {BigInt} amount - The amount to be withdrawn
  * @param {Object} overrides - Transaction overrides
- * @param {Object} signerData - Signer data used to send the transaction.
+ * @param {Object} signerData - Signer data used to send the transaction. Optional
  * @param {String} providerUrl - Network url (i.e, http://localhost:8545). Optional
- * @param {Boolean} isInstant - Whether it should be an Instant Withdrawal
+ * @param {Boolean} isInstant - Whether it should be an Instant Withdrawal. Optional
  * @returns {Number} estimated gas for the withdrawCircuit
  */
 async function estimateWithdrawCircuitGasLimit (token, amount, overrides, signerData, providerUrl, isInstant = true) {
